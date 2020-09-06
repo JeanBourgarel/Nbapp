494403753a0b90f065df8c854ce0be9eeee55731
Jean <jean.bourgarel@epitech.eu>
2020-09-06 16:05:05 +0200

    fixed bug from new test fragment

--- app/src/main/java/com/example/nbapp/ui/test/TestFragment.kt
+++ app/src/main/java/com/example/nbapp/ui/test/TestFragment.kt
@@ -12,18 +12,18 @@ import com.example.nbapp.R
 
 class TestFragment : Fragment() {
 
-    private lateinit var homeViewModel: TestViewModel
+    private lateinit var testViewModel: TestViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
+        testViewModel =
             ViewModelProviders.of(this).get(TestViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        val root = inflater.inflate(R.layout.fragment_test, container, false)
+        val textView: TextView = root.findViewById(R.id.text_test)
+        testViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
