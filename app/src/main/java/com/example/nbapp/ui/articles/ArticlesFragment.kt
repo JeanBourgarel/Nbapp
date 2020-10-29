c7ffe98faf29268fa894beefde2103528cfd5925
Jean <jean.bourgarel@epitech.eu>
2020-10-29 17:20:06 +0100

    refactoring

--- app/src/main/java/com/example/nbapp/ui/articles/ArticlesFragment.kt
+++ app/src/main/java/com/example/nbapp/ui/articles/ArticlesFragment.kt
@@ -1,23 +1,22 @@
-package com.example.nbapp.ui.slideshow
+package com.example.nbapp.ui.articles
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.nbapp.R
 
-class SlideshowFragment : Fragment() {
+class ArticlesFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
+//    private lateinit var articlesViewModel: ArticlesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val root = inflater.inflate(R.layout.fragment_articles, container, false)
         return root
     }
 }
--- app/src/main/java/com/example/nbapp/ui/players/PlayersFragment.kt
+++ app/src/main/java/com/example/nbapp/ui/players/PlayersFragment.kt
@@ -1,21 +1,20 @@
-package com.example.nbapp.ui.gallery
+package com.example.nbapp.ui.players
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.nbapp.R
 
-class GalleryFragment : Fragment() {
+class PlayersFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val root = inflater.inflate(R.layout.fragment_players, container, false)
         return root
     }
 }
